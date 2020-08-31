c1b4c54c26e8482ea9381d6258e20e739bd8dcea
Stephen Buckler <stephen.e.buckler@gmail.com>
2020-08-30 21:15:47 -0400

    Add splash screen

--- states/start-game.js
+++ states/start-game.js
@@ -5,6 +5,7 @@ export class StartGame {
     constructor(game, states, transition) {
         this.game = game;
         this.playingState = states.PLAYING;
+        this.splashScreenImage = document.getElementById("splashScreen");
         this.transition = transition;
     }
 
@@ -17,11 +18,12 @@ export class StartGame {
     }
 
     render(ctx) {
-        ctx.fillStyle = "#0c0";
+        ctx.drawImage(this.splashScreenImage, 0, 0);
+        ctx.fillStyle = "#fff";
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
         ctx.font = "48px mono";
-        ctx.fillText("Gator Golfer!", this.game.width / 2, this.game.height / 2);
+        ctx.fillText("Gator Golfer!", this.game.width / 2, this.game.height / 2 - 64);
         ctx.font = "32px mono";
         ctx.fillText("Press ENTER", this.game.width / 2, this.game.height / 2 + 128);
     }
